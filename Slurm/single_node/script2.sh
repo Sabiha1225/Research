f14348eaa9b9710f550671cf1ee71aea440bce84
Sabiha Afroz <1015052012.sa@grad.cse.buet.ac.bd>
2023-03-22 18:28:05 -0400

    Update script2.sh

--- Slurm/single_node/script2.sh
+++ Slurm/single_node/script2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash --login
 
 # Specify the queue (also known as a partition)
-#SBATCH --partition=amd
+#SBATCH --partition=LocalQ
 
 # run a single task, using a single CPU core
-#SBATCH --ntasks=1
+#SBATCH --ntasks=4
 
-#SBATCH --output=myScript.o%J
+#SBATCH --output=script2.o%J
 
 #run a program command to print hostname and uptime
 /bin/hostname && /bin/uptime
